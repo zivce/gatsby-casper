d605819801fafcedc5179cd57990bf03d57cf51d
zivce <zivkovicmilos5@hotmail.com>
2018-12-21 23:05:09 +0100

    - Changes to config yml.

--- src/cms-ext.js
+++ src/cms-ext.js
@@ -4,6 +4,25 @@ init({
     config: {
         backend: {
             name: "git-gateway",
+            publish_mode: "editorial_workflow",
+            media_folder: "static",
+            media_library: ""
         },
-    },
+        collections: {
+            name: "post",
+            label: "Post",
+            folder: "content",
+            create: "true",
+            fields: [
+                {
+                    label: "Layout",
+                    name: "layout",
+                    widget: "hidden",
+                    default: "post"
+                },
+                { label: "Title", name: "title", widget: "string" },
+                { label: "Featured Image", name: "thumbnail", widget: "image" },
+                { label: "Body", name: "body", widget: "markdown" }]
+        }
+    }
 })
