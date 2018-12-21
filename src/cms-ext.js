8c03711ed01bacd5c1bc5867dfdec45ea5b01a28
zivce <zivkovicmilos5@hotmail.com>
2018-12-21 23:09:30 +0100

    - Changed cms.

--- src/cms-ext.js
+++ src/cms-ext.js
@@ -2,13 +2,13 @@ import CMS, { init } from "netlify-cms"
 
 init({
     config: {
+        media_folder: "static",
+        media_library: "",
         backend: {
             name: "git-gateway",
             publish_mode: "editorial_workflow",
-            media_folder: "static",
-            media_library: ""
         },
-        collections: {
+        collections: [{
             name: "post",
             label: "Post",
             folder: "content",
@@ -23,6 +23,6 @@ init({
                 { label: "Title", name: "title", widget: "string" },
                 { label: "Featured Image", name: "thumbnail", widget: "image" },
                 { label: "Body", name: "body", widget: "markdown" }]
-        }
+        }]
     }
 })
